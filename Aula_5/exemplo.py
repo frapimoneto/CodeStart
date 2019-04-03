e4811404d133304a0c9629d57d44975b9b29bbb4
Francisco Pinto Moreira Neto <31639140+frapimoneto@users.noreply.github.com>
2019-04-03 14:33:44 -0300

    Add files via upload

---
+++ Aula_5/exemplo.py
@@ -0,0 +1,6 @@
+import random
+import turtle
+turtle.bgcolor("white")
+turtle.title("Sorteio")
+random.seed(5)
+print(random.randint(0,10))
---
+++ Aula_5/exemplo_click.py
@@ -0,0 +1,5 @@
+import turtle
+def diz_opa(x, y):
+    print ("opa!")
+
+turtle.onScreenClick(diz_opa)
